6581fd9c00bdf3f6d465c67a8a9bc938c1b6e8de
Joan Domingo <joands@gmail.com>
2016-07-24 21:32:27 +0200

    notifications fixes

--- app/src/main/java/cat/xojan/random1/ui/controller/NotificationController.java
+++ app/src/main/java/cat/xojan/random1/ui/controller/NotificationController.java
@@ -10,6 +10,7 @@ import javax.inject.Inject;
 
 import cat.xojan.random1.R;
 import cat.xojan.random1.domain.entity.Podcast;
+import cat.xojan.random1.ui.activity.RadioPlayerActivity;
 
 public class NotificationController {
 
@@ -22,16 +23,19 @@ public class NotificationController {
 
     public void showNotification(Class<?> cls, int notificationId, Context ctx, Podcast podcast) {
 
-        final Intent notificationIntent = new Intent(ctx, cls);
-        notificationIntent.setAction(Intent.ACTION_MAIN);
+        final Intent notificationIntent = new Intent(ctx, RadioPlayerActivity.class);
+        /*notificationIntent.setAction(Intent.ACTION_MAIN);
         notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0, notificationIntent, 0);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);*/
+        //notificationIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(ctx)
                         .setSmallIcon(R.drawable.ic_podcast_notification)
                         .setContentTitle(ctx.getString(R.string.app_name))
-                        .setContentText(podcast.category() + " " + podcast.description());
+                        .setContentText(podcast.category() + " " + podcast.description())
+                        .setOngoing(true);
 
         builder.setContentIntent(contentIntent);
         mNotificationManager.notify(notificationId, builder.build());
