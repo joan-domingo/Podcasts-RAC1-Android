11d2979a1c183b541a52942e1474e2563d4ab3db
Joan Domingo <joands@gmail.com>
2016-12-17 09:46:00 +0100

    remove old podcasts at first run

--- app/src/main/java/cat/xojan/random1/Application.java
+++ app/src/main/java/cat/xojan/random1/Application.java
@@ -1,8 +1,14 @@
 package cat.xojan.random1;
 
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
 import com.squareup.leakcanary.LeakCanary;
+
+import java.io.File;
 
 import cat.xojan.random1.injection.component.AppComponent;
 import cat.xojan.random1.injection.component.DaggerAppComponent;
@@ -11,6 +17,7 @@ import io.fabric.sdk.android.Fabric;
 
 public class Application extends android.app.Application {
 
+    private static final String TAG = Application.class.getSimpleName();
     private AppComponent mComponent;
 
     @Override
@@ -19,6 +26,8 @@ public class Application extends android.app.Application {
         initCrashlytics();
         initInjector();
         initLeakDetection();
+
+        removePreviousDownloadedPodcasts();
     }
 
     public AppComponent getAppComponent() {
@@ -40,6 +49,38 @@ public class Application extends android.app.Application {
     private void initCrashlytics() {
         if (!BuildConfig.DEBUG) {
             Fabric.with(this, new Answers(), new Crashlytics());
+        }
+    }
+
+    private void removePreviousDownloadedPodcasts() {
+        boolean arePodcastsRemoved = getSharedPreferences("preferences", Context.MODE_PRIVATE)
+                .getBoolean("old_downloaded_podcasts", false);
+        if (!arePodcastsRemoved) {
+            Log.d(TAG, "removing old podcasts");
+
+            removeDownloaded();
+            removeDownloading();
+
+            getSharedPreferences("preferences", Context.MODE_PRIVATE).edit()
+                    .putBoolean("old_downloaded_podcasts", true).apply();
+        }
+    }
+
+    private void removeDownloaded() {
+        File iternalFileDirectory = getExternalFilesDir(Environment.DIRECTORY_PODCASTS);
+        for (File podcastFile : iternalFileDirectory.listFiles()) {
+            if (podcastFile.delete()) {
+                Log.d(TAG, "delete downloaded podcast");
+            }
+        }
+    }
+
+    private void removeDownloading() {
+        File iternalFileDirectory = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        for (File podcastFile : iternalFileDirectory.listFiles()) {
+            if (podcastFile.delete()) {
+                Log.d(TAG, "delete downloading podcast");
+            }
         }
     }
 }
