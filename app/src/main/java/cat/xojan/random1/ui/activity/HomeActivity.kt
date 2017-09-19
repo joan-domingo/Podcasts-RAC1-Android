ab2b17064a9cabcccfae9b84304b49653330b2b9
Joan Domingo <joands@gmail.com>
2017-09-19 17:37:24 +0200

    Migrate the whole Home Activity to Kotlin

--- app/src/main/java/cat/xojan/random1/ui/activity/HomeActivity.kt
+++ app/src/main/java/cat/xojan/random1/ui/activity/HomeActivity.kt
@@ -1,20 +1,36 @@
 package cat.xojan.random1.ui.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import cat.xojan.random1.R
 import cat.xojan.random1.injection.HasComponent
 import cat.xojan.random1.injection.component.DaggerHomeComponent
 import cat.xojan.random1.injection.component.HomeComponent
 import cat.xojan.random1.injection.module.HomeModule
 import cat.xojan.random1.ui.adapter.HomePagerAdapter
-import cat.xojan.random1.ui.fragment.DownloadsFragment
-import cat.xojan.random1.ui.fragment.ProgramFragment
+import cat.xojan.random1.ui.fragment.*
+import cat.xojan.random1.viewmodel.PodcastsViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_home.*
+import javax.inject.Inject
 
 class HomeActivity() : BaseActivity(), HasComponent<HomeComponent> {
     companion object {
         private val PERMISSION_WRITE_EXTERNAL_STORAGE = 20
     }
+
+    @Inject internal lateinit var mViewModel: PodcastsViewModel
+    private val mCompositeDisposable = CompositeDisposable()
 
     override val component: HomeComponent by lazy {
         DaggerHomeComponent.builder()
@@ -42,5 +58,68 @@ class HomeActivity() : BaseActivity(), HasComponent<HomeComponent> {
 
         viewPager.adapter = pageAdapter
         tabLayout.setupWithViewPager(viewPager)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_export_podcasts -> if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestWriteExternalStoragePermission()
+            } else {
+                exportPodcasts()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_WRITE_EXTERNAL_STORAGE -> {
+                exportPodcasts()
+            }
+        }
+    }
+
+    private fun requestWriteExternalStoragePermission() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_WRITE_EXTERNAL_STORAGE)
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            var fragment: Fragment? = getFragment(HourByHourListFragment.TAG)
+            if (fragment == null) fragment = getFragment(SectionFragment.TAG)
+
+            if (fragment != null && supportFragmentManager
+                    .findFragmentByTag(PodcastListFragment.TAG) == null) {
+                if ((fragment as BaseFragment).handleOnBackPressed()) {
+                    return
+                }
+            }
+        }
+        super.onBackPressed()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mCompositeDisposable.clear()
+    }
+
+    private fun exportPodcasts() {
+        mCompositeDisposable.add(mViewModel.exportPodcasts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(Consumer<Boolean> { this.notifyUser(it) }))
+    }
+
+    private fun notifyUser(b: Boolean?) {
+        Toast.makeText(this, getString(R.string.podcasts_exported), Toast.LENGTH_LONG).show()
     }
 }
