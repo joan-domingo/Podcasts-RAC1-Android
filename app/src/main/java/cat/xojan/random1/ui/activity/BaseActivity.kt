a0ad55ecda6a8463fd10065887c83607e3f77bf4
Joan Domingo <joands@gmail.com>
2017-09-24 10:46:27 +0200

    Migrate Splash and Base activity

---
+++ app/src/main/java/cat/xojan/random1/ui/activity/BaseActivity.kt
@@ -0,0 +1,45 @@
+package cat.xojan.random1.ui.activity
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import cat.xojan.random1.Application
+import cat.xojan.random1.R
+import cat.xojan.random1.injection.component.AppComponent
+import cat.xojan.random1.injection.module.BaseActivityModule
+
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    /**
+     * Get the Main Application component for dependency injection.
+     *
+     * @return [AppComponent]
+     */
+    protected val applicationComponent: AppComponent
+        get() = (application as Application).appComponent
+
+    /**
+     * Get an Activity module for dependency injection.
+     *
+     * @return [cat.xojan.random1.injection.component.BaseActivityComponent]
+     */
+    protected val activityModule: BaseActivityModule
+        get() = BaseActivityModule(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        applicationComponent.inject(this)
+    }
+
+    /**
+     * Adds a [Fragment] to this activity's layout.
+     */
+    fun addFragment(fragment: Fragment, tag: String,
+                    addToBackSTack: Boolean) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container_fragment, fragment, tag)
+        if (addToBackSTack) fragmentTransaction.addToBackStack(tag)
+        fragmentTransaction.commit()
+    }
+}
---
+++ app/src/main/java/cat/xojan/random1/ui/activity/SplashActivity.kt
@@ -0,0 +1,17 @@
+package cat.xojan.random1.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
