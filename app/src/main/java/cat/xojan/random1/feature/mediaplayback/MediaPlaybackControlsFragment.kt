35aaab7e8d22fa316ce8a57c5a8097976ebc0608
Joan Domingo <joands@gmail.com>
2017-12-10 23:52:37 +0100

    add logic to button in controls fragment

--- app/src/main/java/cat/xojan/random1/feature/mediaplayback/MediaPlaybackControlsFragment.kt
+++ app/src/main/java/cat/xojan/random1/feature/mediaplayback/MediaPlaybackControlsFragment.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cat.xojan.random1.R
+import kotlinx.android.synthetic.main.fragment_playback_controls.*
 
 class MediaPlaybackControlsFragment : Fragment() {
 
@@ -19,6 +20,24 @@ class MediaPlaybackControlsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_playback_controls, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        play_pause.setOnClickListener {
+            val controller = MediaControllerCompat.getMediaController(activity as Activity)
+            val playbackState = controller.playbackState
+            val state = playbackState?.state ?: PlaybackStateCompat.STATE_NONE
+            if (state == PlaybackStateCompat.STATE_PAUSED ||
+                    state == PlaybackStateCompat.STATE_STOPPED ||
+                    state == PlaybackStateCompat.STATE_NONE) {
+                playMedia()
+            } else if (state == PlaybackStateCompat.STATE_PLAYING ||
+                    state == PlaybackStateCompat.STATE_BUFFERING ||
+                    state == PlaybackStateCompat.STATE_CONNECTING) {
+                pauseMedia()
+            }
+        }
     }
 
     override fun onStart() {
@@ -49,13 +68,34 @@ class MediaPlaybackControlsFragment : Fragment() {
         if (activity == null || metadata == null) {
             return
         }
+        title.text = metadata.description.title
     }
 
-    private fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
-        Log.d(TAG, "onPlaybackStateChanged " + state)
-        if (activity == null || state == null) {
+    private fun onPlaybackStateChanged(playbackState: PlaybackStateCompat?) {
+        Log.d(TAG, "onPlaybackStateChanged " + playbackState)
+        if (activity == null || playbackState == null) {
             return
         }
+        val state = playbackState.state
+        if (state == PlaybackStateCompat.STATE_PAUSED ||
+                state == PlaybackStateCompat.STATE_STOPPED ||
+                state == PlaybackStateCompat.STATE_NONE) {
+            play_pause.setImageResource(R.drawable.ic_play_arrow)
+        } else if (state == PlaybackStateCompat.STATE_PLAYING ||
+                state == PlaybackStateCompat.STATE_BUFFERING ||
+                state == PlaybackStateCompat.STATE_CONNECTING) {
+            play_pause.setImageResource(R.drawable.ic_pause)
+        }
+    }
+
+    private fun playMedia() {
+        val controller = MediaControllerCompat.getMediaController(activity as Activity)
+        controller?.transportControls?.play()
+    }
+
+    private fun pauseMedia() {
+        val controller = MediaControllerCompat.getMediaController(activity as Activity)
+        controller?.transportControls?.pause()
     }
 
     // Receive callbacks from the MediaController. Here we update our state such as which queue
