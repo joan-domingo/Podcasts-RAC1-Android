0447aa8e74a707681b04a5790790deab8747e9fe
Joan Domingo <joands@gmail.com>
2017-12-25 13:09:24 +0100

    set up crash reporting

--- app/src/main/java/cat/xojan/random1/domain/model/CrashReporter.kt
+++ app/src/main/java/cat/xojan/random1/domain/model/CrashReporter.kt
@@ -1,11 +1,15 @@
 package cat.xojan.random1.domain.model
 
+import com.crashlytics.android.Crashlytics
+
 
 class CrashReporter {
 
     fun logException(e: Throwable) {
+        Crashlytics.logException(e)
     }
 
     fun logException(message: String) {
+        Crashlytics.log(message)
     }
 }
