7cb99d18f4f8c9dfeebc90285bc7703d153f0e75
Joan Domingo <joands@gmail.com>
2018-01-15 20:07:33 +0100

    change notification media player icons

--- app/src/main/java/cat/xojan/random1/feature/notification/NotificationManager.kt
+++ app/src/main/java/cat/xojan/random1/feature/notification/NotificationManager.kt
@@ -225,7 +225,7 @@ class NotificationManager(private val service: MediaPlaybackService): BroadcastR
         var playPauseButtonPosition = 0
         // If skip to previous action is enabled
         if ((mPlaybackState!!.actions and PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS) != 0L) {
-            notificationBuilder.addAction(android.R.drawable.ic_media_previous,
+            notificationBuilder.addAction(R.drawable.ic_skip_previous_white_24px,
                     service.getString(R.string.label_previous), previousIntent)
 
             // If there is a "skip to previous" button, the play/pause button will
@@ -241,18 +241,18 @@ class NotificationManager(private val service: MediaPlaybackService): BroadcastR
         val intent: PendingIntent
         if (mPlaybackState?.state == PlaybackStateCompat.STATE_PLAYING) {
             label = service.getString(R.string.label_pause)
-            icon = android.R.drawable.ic_media_pause
+            icon = R.drawable.ic_pause
             intent = pauseIntent
         } else {
             label = service.getString(R.string.label_play)
-            icon = android.R.drawable.ic_media_play
+            icon = R.drawable.ic_play_arrow
             intent = playIntent
         }
         notificationBuilder.addAction(NotificationCompat.Action(icon, label, intent))
 
         // If skip to next action is enabled
         if ((mPlaybackState!!.actions and PlaybackStateCompat.ACTION_SKIP_TO_NEXT) != 0L) {
-            notificationBuilder.addAction(android.R.drawable.ic_media_next,
+            notificationBuilder.addAction(R.drawable.ic_skip_next_white_24px,
                     service.getString(R.string.label_next), nextIntent)
         }
 
