1c9703904b5e1f2d3e1d7bb2691c91dfef70e4d2
Joan Domingo <joands@gmail.com>
2018-01-25 20:23:50 +0100

    inject eventLogger to BrowserModule

--- app/src/main/java/cat/xojan/random1/injection/module/BrowseModule.kt
+++ app/src/main/java/cat/xojan/random1/injection/module/BrowseModule.kt
@@ -3,6 +3,7 @@ package cat.xojan.random1.injection.module
 import android.app.Activity
 import cat.xojan.random1.domain.interactor.PodcastDataInteractor
 import cat.xojan.random1.domain.interactor.ProgramDataInteractor
+import cat.xojan.random1.domain.model.EventLogger
 import cat.xojan.random1.feature.browser.BrowserViewModel
 import dagger.Module
 import dagger.Provides
@@ -13,8 +14,9 @@ class BrowseModule(private val activity: Activity) {
     @Provides
     fun provideBrowserViewModel(
             podcastInteractor: PodcastDataInteractor,
-            programInteractor: ProgramDataInteractor
+            programInteractor: ProgramDataInteractor,
+            eventLogger: EventLogger
             ): BrowserViewModel {
-        return BrowserViewModel(podcastInteractor, programInteractor)
+        return BrowserViewModel(podcastInteractor, programInteractor, eventLogger)
     }
 }
