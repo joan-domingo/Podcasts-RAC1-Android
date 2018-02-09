82ab4c8f53e7011642b9ede15dfc1597dff29afa
Joan Domingo <joands@gmail.com>
2018-02-09 23:04:12 +0100

    fix tests

--- app/src/test/java/cat/xojan/random1/testutil/Data.kt
+++ app/src/test/java/cat/xojan/random1/testutil/Data.kt
@@ -6,11 +6,11 @@ import cat.xojan.random1.domain.model.*
 import java.util.*
 
 val podcast1 = Podcast(Audio("audioId"), "path1", "filePath1", Date(), 0, "podcastId1",
-        null, null, Podcast.State.DOWNLOADED, "programTitle1", 1)
+        null, null, PodcastState.DOWNLOADED, "programTitle1", 1)
 val podcast2 = Podcast(Audio("audioId"), "path1", null, Date(), 0, "podcastId2", null,
-        null, Podcast.State.LOADED, "programTitle1", 1)
+        null, PodcastState.LOADED, "programTitle1", 1)
 val podcast3 = Podcast(Audio("audioId"), "path1", "filePath3", Date(), 0, "podcastId3",
-        null, null, Podcast.State.DOWNLOADING, "programTitle1", 1)
+        null, null, PodcastState.DOWNLOADING, "programTitle1", 1)
 val podcastList = listOf(podcast1, podcast2, podcast3)
 
 val section1 = Section("sectionId1", "sectionTitle1", "http://image.url", true, SectionType
