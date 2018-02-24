e9488a80e8dd5b27018d97a079d6b3db7616a910
Joan Domingo <joands@gmail.com>
2018-02-24 17:24:49 +0100

    clean up code

--- app/src/catradio/java/cat/xojan/random1/domain/model/Items.kt
+++
@@ -1,7 +0,0 @@
-package cat.xojan.random1.domain.model
-
-import com.squareup.moshi.Json
-
-data class Items(
-        @Json(name = "lletra") val lletres: List<Lletra>
-)
--- app/src/catradio/java/cat/xojan/random1/domain/model/ProgramData.kt
+++ app/src/catradio/java/cat/xojan/random1/domain/model/ProgramData.kt
@@ -2,7 +2,34 @@ package cat.xojan.random1.domain.model
 
 import com.squareup.moshi.Json
 
-data class ProgramData(
-        @Json(name = "resposta") val resposta: Resposta,
-        val programs: List<Program> = listOf()
+class ProgramData(
+        @Json(name = "resposta") val resposta: Resposta
+
+) {
+    val programs: List<Program> = listOf()
+}
+
+class Resposta(
+        @Json(name ="items") val items: Items
+)
+
+class Items(
+        @Json(name = "lletra") val lletres: List<Lletra>
+)
+
+class Lletra(
+        @Json(name="valor") val valor: Char,
+        @Json(name="item") val items: List<Item>
+)
+
+class Item(
+        @Json(name = "lletra") val lletra: Char,
+        @Json(name = "presentador") val presentador: String? = null,
+        @Json(name = "url_podcast") val urlPodcast: String? = null,
+        @Json(name = "id") val id: String,
+        @Json(name = "titol") val titol: String,
+        @Json(name = "data_publicacio") val dataPublicacio: String,
+        @Json(name = "domini") val domini: String
+        //@Json(name = "seccions") val seccions: Seccio,
+        //@Json(name = "imatges") val imatges: Imatge,
 )
